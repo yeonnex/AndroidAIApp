c38affbea5c99dff48a0c797030ae61eb28f962d
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-28 10:38:19 +0900

    AiApp Camera ImageCapture 구현 - 테스트 오류...😥 - 참고 링크 - 카메라 HAL3 - https://source.android.com/devices/camera/camera3?hl=ko

--- app/src/main/java/com/yeonnex/aiapp/MainActivity.kt
+++ app/src/main/java/com/yeonnex/aiapp/MainActivity.kt
@@ -1,24 +1,37 @@
 package com.yeonnex.aiapp
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.yeonnex.aiapp.databinding.ActivityMainBinding
 import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "==================="
     private lateinit var binding: ActivityMainBinding
-
     private val REQUIRED_PERMISSIONS: String = Manifest.permission.CAMERA
     private val REQUEST_CODE_PERMISSIONS = 10
-    private val imageCapture: ImageCapture? = null
-
+    private var imageCapture: ImageCapture? = null
     private var outputDirectory: File? = null
     private var cameraExecutor: ExecutorService? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +56,50 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
+        if (imageCapture == null) { Log.d(TAG, "imageCapture is null...") }
+        else Log.d(TAG, "imageCaptue is NOT... NULL....")
+        val imageCapture = imageCapture?: return
+        val photoFile = File(outputDirectory, SimpleDateFormat(FILENAME_FORMAT,  Locale.KOREA).format(System.currentTimeMills()) + ".jpg")
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOption, ContextCompat.getMainExecutor(this),
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exception: ImageCaptureException) {
+                exception.printStackTrace()
+                Log.e("=======", "ImageCaptureException ${e.message}")
+            }
+
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                Toast.makeText(baseContext, "success: $savedUri", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 
-    private fun startCamera() {
+    private fun startCamera() { // 얘도 지연 요청을 해야 함
+        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+            ProcessCameraProvider.getInstance(this)
+        val preview = Preview.Builder().build().also{
+            it.setSurfaceProvider(viewFinder.surfaceProvider)
+        }
+        imageCapture = ImageCapture.Builder().build() // 빌더 패턴
+        val camaraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        try{
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture )
+        }catch (e:Exception){ e.printStackTrace() }
+    }, ContextCompat.getMainExecutor(this))
+
+}
+    private fun bindPreview(camaraProvider: ProcessCameraProvider){
+        val preview: Preview = Preview.Builder().build()
+        preview.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview)
     }
+
 
     private fun getOutputDirectory(): File? {
         return File(externalMediaDirs[0], System.currentTimeMillis().toString() + ".jpg")
