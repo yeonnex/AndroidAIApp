c90c0609df916678d4c7fd01299a7e8b3592508e
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-28 11:49:06 +0900

    AiApp 리팩토링

--- app/src/main/java/com/yeonnex/aiapp/MainActivity.kt
+++ app/src/main/java/com/yeonnex/aiapp/MainActivity.kt
@@ -20,121 +20,109 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
+
 class MainActivity : AppCompatActivity() {
 
-    private val TAG = "==================="
-    private lateinit var binding: ActivityMainBinding
-    private val REQUIRED_PERMISSIONS: String = Manifest.permission.CAMERA
-    private val REQUEST_CODE_PERMISSIONS = 10
+    companion object {
+        private const val TAG = "==========="
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+
     private var imageCapture: ImageCapture? = null
-    private var outputDirectory: File? = null
-    private var cameraExecutor: ExecutorService? = null
-    private var imageAnalysis: ImageAnalysis? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        if (allPermissionsGranted()) {
-            // 카메라 시작
+        if (allPermissionsGranted())
             startCamera()
-        } else {
+        else
             ActivityCompat.requestPermissions(
-                this, arrayOf(REQUIRED_PERMISSIONS), REQUEST_CODE_PERMISSIONS)
-        }
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
 
-        binding.cameraCaptureButton.setOnClickListener {
-            takePhoto()
-        }
-
+        camera_capture_button.setOnClickListener {  takePhoto()  }
         outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     private fun takePhoto() {
-        if (imageCapture == null) { Log.d(TAG, "imageCapture is null...") }
-        else Log.d(TAG, "imageCaptue is NOT... NULL....")
-        val imageCapture = imageCapture?: return
-        val photoFile = File(outputDirectory, SimpleDateFormat(FILENAME_FORMAT,  Locale.KOREA).format(System.currentTimeMills()) + ".jpg")
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = File(outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.KOREA )
+                .format(System.currentTimeMillis()) + ".jpg")
         val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
         imageCapture.takePicture(outputOption, ContextCompat.getMainExecutor(this),
-        object : ImageCapture.OnImageSavedCallback {
-            override fun onError(exception: ImageCaptureException) {
-                exception.printStackTrace()
-                Log.e("=======", "ImageCaptureException ${e.message}")
-            }
-
-            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                val savedUri = Uri.fromFile(photoFile)
-                Toast.makeText(baseContext, "success: $savedUri", Toast.LENGTH_LONG).show()
-            }
-        })
-    }
-
-    private fun startCamera() { // 얘도 지연 요청을 해야 함
-        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
-            ProcessCameraProvider.getInstance(this)
-        val preview = Preview.Builder().build().also{
-            it.setSurfaceProvider(viewFinder.surfaceProvider)
-        }
-        imageCapture = ImageCapture.Builder().build() // 빌더 패턴
-        val imageAnalysis: ImageAnalysis? = ImageAnalysis.Builder().build().also {
-            it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                Log.d(TAG, "luminosity : $luma")
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(e: ImageCaptureException) {
+                    Log.e(TAG, "ImageCaptureException ${e.message}", e)
+                }
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Log.e(TAG, "ImageCaptureException success")
+                    val savedUri = Uri.fromFile(photoFile)
+                    Toast.makeText(baseContext, "success:$savedUri", Toast.LENGTH_LONG).show()
+                }
             })
-        }
-        val camaraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-        try{
-            cameraProvider.unbindAll()
-            cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture )
-        }catch (e:Exception){ e.printStackTrace() }
-    }, ContextCompat.getMainExecutor(this))
-
-}
-    private fun bindPreview(camaraProvider: ProcessCameraProvider){
-        val preview: Preview = Preview.Builder().build()
-        preview.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-        cameraProvider.unbindAll()
-        cameraProvider.bindToLifecycle(this, cameraSelector, preview)
     }
 
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-    private fun getOutputDirectory(): File? {
-        return File(externalMediaDirs[0], System.currentTimeMillis().toString() + ".jpg")
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(viewFinder.surfaceProvider)  }
+
+            imageCapture = ImageCapture.Builder().build()
+            val imageAnalysis: ImageAnalysis? = ImageAnalysis.Builder().build().also {
+                it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                    Log.d(TAG, "luminosity: $luma")  })   }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, imageAnalysis)
+            } catch (e: Exception) { e.printStackTrace()}
+        }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun allPermissionsGranted(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this, REQUIRED_PERMISSIONS) == PackageManager.PERMISSION_GRANTED
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if(mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            this, it) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray ) {
 
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
+            if (allPermissionsGranted())
                 startCamera()
-            } else {
+            else {
                 Toast.makeText(this,
                     "Permissions not granted by the user.", Toast.LENGTH_LONG).show()
                 finish()
             }
         }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        cameraExecutor?.shutdown()
+        cameraExecutor.shutdown()
     }
 }
-
-
-
-
