0081d8975535d850fc0cc5e977b439f14f58f9ea
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-29 13:47:17 +0900

    AiApp TensorFlow Lite 적용 - 손글씨 분류 모델

--- app/src/main/java/com/yeonnex/aiapp/PhotoActivity.kt
+++ app/src/main/java/com/yeonnex/aiapp/PhotoActivity.kt
@@ -7,9 +7,12 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.renderscript.Element
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_photo.*
 import java.io.File
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 
 class PhotoActivity : AppCompatActivity() {
@@ -41,9 +44,35 @@ class PhotoActivity : AppCompatActivity() {
             try {
                 var bimap: Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
                 imageView.setImageBitmap(bitmap)
+                val resized_bitmap = resizeBitmap(bitmap)
+                processTFLight(this, toGrayByteBuffer(resized_bitmap)) // 비트맵을 바이트 버퍼로 만드는 법...?
             }catch (e:Exception) { Log.e(TAG, "getBitmapException ${e.message}", e)
             }
         }
+    }
+    private fun resizeBitmap(bitmap: Bitmap):Bitmap{
+        bitmap.createScaleBitmap(bitmap, 28, 28,false)
+    }
+    pvivate fun processTFLight(context: Context, byteBuffer: ByteBuffer){
+        val model = KerasModel.newInstance(context)
+
+        val inputFeatured0 = TensorBuffer.createFixelSize(intArrayOf(1,28,28), Element.DataType.FLOAT_32)
+
+    }
+    private fun toGrayByteBuffer(bitmap: Bitmap): ByteBuffer {
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(28,28, ResizeOp.ResizedMethod.BILENEAR))
+        val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(bitmap.byteCount)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        val pixels = intArrayOf(bitmap.width * bitmap*height) // 리스트와 배열의 자료구조는 다름!!
+        bitmap.getPixels(pixels, 0, bitmap.width, 0,0,bitmap.width, bitmap.height)
+
+        pixels.forEach{pixel ->
+            val r = pixel >> 16 & 0xFF
+        }
+
+        return byteBuffer
     }
 
     //Uri -> Path(파일경로)
